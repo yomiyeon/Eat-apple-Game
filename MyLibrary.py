240baa17ee3369ac448ee9e2d16bb352a06eff18
이서연 <seoyoen0213@naver.com>
2021-01-21 21:29:31 +0900

    번역

--- MyLibrary.py
+++ MyLibrary.py
@@ -4,17 +4,17 @@ import sys, time, random, math, pygame
 from pygame.locals import *
 
 
-# prints text using the supplied font
+# 제공된 글꼴을 사용하여 텍스트 인쇄
 def print_text(font, x, y, text, color=(255,255,255)):
     imgText = font.render(text, True, color)
-    screen = pygame.display.get_surface() #req'd when function moved into MyLibrary
+    screen = pygame.display.get_surface() #기능이 My Library로 이동했을 때
     screen.blit(imgText, (x,y))
 
-# MySprite class extends pygame.sprite.Sprite
+#MySprite 클래스가 확장한다 pygame.sprite.Sprite
 class MySprite(pygame.sprite.Sprite):
     
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self) #extend the base Sprite class
+        pygame.sprite.Sprite.__init__(self) #기본 스프라이트 클래스를 확장합니다.
         self.master_image = None
         self.frame = 0
         self.old_frame = -1
@@ -27,17 +27,17 @@ class MySprite(pygame.sprite.Sprite):
         self.direction = 0
         self.velocity = Point(0.0,0.0) 
 
-    #X property
+    #X 속성
     def _getx(self): return self.rect.x
     def _setx(self,value): self.rect.x = value
     X = property(_getx,_setx)
 
-    #Y property
+    #Y 속성
     def _gety(self): return self.rect.y
     def _sety(self,value): self.rect.y = value
     Y = property(_gety,_sety)
 
-    #position property
+    # 위치속성
     def _getpos(self): return self.rect.topleft
     def _setpos(self,pos): self.rect.topleft = pos
     position = property(_getpos,_setpos)
@@ -49,19 +49,19 @@ class MySprite(pygame.sprite.Sprite):
         self.frame_height = height
         self.rect = Rect(0,0,width,height)
         self.columns = columns
-        #try to auto-calculate total frames
+        #전체 프레임을 자동 조절하려고 합니다
         rect = self.master_image.get_rect()
         self.last_frame = (rect.width // width) * (rect.height // height) - 1
 
     def update(self, current_time, rate=30):
-        #update animation frame number
+        #애니메이션 프레임 번호 업데이트
         if current_time > self.last_time + rate:
             self.frame += 1
             if self.frame > self.last_frame:
                 self.frame = self.first_frame
             self.last_time = current_time
 
-        #build current frame only if it changed
+        #변경된 경우에만 현재 프레임 작성
         if self.frame != self.old_frame:
             frame_x = (self.frame % self.columns) * self.frame_width
             frame_y = (self.frame // self.columns) * self.frame_height
@@ -81,12 +81,12 @@ class Point(object):
         self.__x = x
         self.__y = y
 
-    #X property
+    #X 속성
     def getx(self): return self.__x
     def setx(self, x): self.__x = x
     x = property(getx, setx)
 
-    #Y property
+    #Y 속성
     def gety(self): return self.__y
     def sety(self, y): self.__y = y
     y = property(gety, sety)
@@ -94,4 +94,3 @@ class Point(object):
     def __str__(self):
         return "{X:" + "{:.0f}".format(self.__x) + \
             ",Y:" + "{:.0f}".format(self.__y) + "}"
-    
